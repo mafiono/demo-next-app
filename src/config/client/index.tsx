a60c583e17b7f7e458e3d8e1b0f9c37a4b374275
iip jaelani <iip@oktagon.id>
2022-04-01 14:58:41 +0700

    update client config

--- src/config/client/index.tsx
+++ src/config/client/index.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios'
-
 const axiosClient = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
